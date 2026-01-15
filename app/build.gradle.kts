a957ae397d081de01d187272c35e7b642f9905c0
Alberto <birtebs@gmail.com>
2026-01-15 18:37:51 +0100

    Release v1.20.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.riffle"
         minSdk = 24
         targetSdk = 34
-versionCode = 2
-        versionName = "1.20.0"
+versionCode = 3
+        versionName = "1.20.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
