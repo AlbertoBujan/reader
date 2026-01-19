e4e8928a86f9fd978029b57f159ec675bbd4cd29
Alberto <birtebs@gmail.com>
2026-01-19 14:18:55 +0100

    rollback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 34
 versionCode = 12
-        versionName = "2.1.6"
+        versionName = "2.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
