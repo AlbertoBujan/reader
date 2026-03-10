082a2d9a3ef43d6e4d2fbb8334710823cf038a9b
Alberto <birtebs@gmail.com>
2026-03-10 18:17:51 +0100

    Release v3.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.boaxente.riffle"
         minSdk = 24
         targetSdk = 35
-versionCode = 62
-        versionName = "3.0.8"
+versionCode = 63
+        versionName = "3.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
